0430b28ab924266c45faa5f7ed571f4a51aff42d
Haraburda Michał <mich1l199403@gmail.com>
2020-06-06 16:42:17 +0200

    10.2 Obsługa podstron

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,50 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    /* We wsłaściwości "pages" znajdź DZIECI, kontenera stron (sekcje id 'order' i 'booking') */
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+
+    }
+    /* Aktywacja pierwszej ze stron zapisanych w 'thisApp' */
+    thisApp.activatePage(pageMatchingHash);
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+    /* Dodaj klase 'active' pasującej podstronie i odbierz tą klasę nie pasującej */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* Dodaj klase 'active' pasującym linkom i odbierz tą klasę nie pasującej */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
     //console.log('thisApp.data: ', thisApp.data);
@@ -37,6 +79,7 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
+    thisApp.initPages();
     thisApp.initData();
   },
 
